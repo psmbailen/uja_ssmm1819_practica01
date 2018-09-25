a0c34ed0f3dcfee54449abe998449eb660e472f6
jccuevas <jccuevas@ujaen.es>
2018-09-25 12:19:50 +0200

    Añadido el fragmento básico

--- app/src/main/java/es/ujaen/labtelema/practica1/MainActivity.java
+++ app/src/main/java/es/ujaen/labtelema/practica1/MainActivity.java
@@ -1,10 +1,11 @@
 package es.ujaen.labtelema.practica1;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentAuth.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //TODO Añadir algo aquí
         Log.d("ARRANCANDO","La aplicación móvil se está iniciando");
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
